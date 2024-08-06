d82d8538551a38c931c2e33a57a53c67a09f0633
Justin Leibach <j@j>
2024-08-06 14:23:30 -0700

    fix combine script

--- combine-JSON.sh
+++ combine-JSON.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-:'
-This script allows a user to merge large amounts of json in a single directory into a signle .json file.
+: 'This script allows a user to merge large amounts of json in a single directory into a signle .json file.
 Using other methods like python scripts and direct use of jq crashed for lack of memory
 This bash script prevents that by iterating through single jq merge processes until completion
 '
@@ -16,9 +15,8 @@ read filepath
 echo -n "What do you want you want your new JSON file to be called (please add .json)?: "
 read filename
 
-#cd $filepath
+temp="ThisIsTemporary1654321.json"
 
-# create empty array
 file_list=()
 
 # iterate through all files in user designated $filepath that are .json files, then append file to the file_list array
@@ -28,14 +26,28 @@ for file in "$filepath"/*.json; do
     fi
 done
 
-echo "Files in the directory:"
+# Debugging output to check the contents of file_list
+echo "Files in file_list:"
+for f in "${file_list[@]}"; do
+    echo "$f"
+done
+
+echo "This may take a while"
+
+
 # iterate through each file in the file list array and combine it into a single json file using jq.
 for file in "${file_list[@]}"; do
-    jq -s '.' $file > $filename
+    jq -s '.' $file >> $temp
 done
+
+# command to remove the all arrays in the JSON file. Necessary to properly filter with jq
+cat $temp | jq '.[]' > $filename
 
 echo "$filename created in current directory"
 
+# delete the temporary file created
+rm $temp
+echo "$temp deleted"
 
 
 
